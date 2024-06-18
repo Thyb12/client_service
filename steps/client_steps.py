f416089a5b62fcf632a1e709c4355a0aab18f564
Thibaut <jobardthibaut12@gmail.com>
2024-06-18 13:06:46 +0200

    init client

---
+++ steps/client_steps.py
@@ -0,0 +1,66 @@
+import os
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from behave import given, when, then
+from api.client_api import create_client, read_clients, delete_client, read_specific_client
+from unittest.mock import patch
+
+# Configuration de la base de données en fonction de la variable d'environnement ENV
+if os.environ.get("ENV") == "test":
+    DATABASE_URL = "sqlite:///./test_db.sqlite"
+else:
+    DATABASE_URL = "sqlite:///./client_api.db"
+
+# Créez une session de base de données
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Initialisez la session de base de données pour les tests
+@pytest.fixture(scope="function")
+def db():
+    db = SessionLocal()
+    yield db
+    db.close()
+
+@given('je crée un client avec le nom "{name}" et la quantité {quantity:d}')
+async def create_product(context, name, quantity):
+    client_data = {"name": name, "quantity": quantity}
+    context.client_created = await create_client(client_data, db())
+
+
+@then('je récupère tous les clients')
+async def get_all_products(context):
+    context.clients = await read_clients(db())
+
+@when('je supprime le client avec l\'ID {product_id:d}')
+async def delete_product(context, product_id):
+    await delete_client(product_id, db())
+
+
+@then('le client est créé')
+def check_product_created(context):
+    assert read_clients().__sizeof__() > 0
+
+@then('je reçois une liste de clients')
+async def check_products_received(context):
+    reponse = await read_clients(db())
+    assert reponse == context.clients
+
+@then('le client est supprimé avec succès')
+async def check_product_deleted(context):
+    assert await delete_client(context.clients[0], db()) is not None
+@then('je reçois le client spécifique avec l\'ID {product_id:d}')
+async def check_specific_product_received(context, product_id):
+    assert read_specific_client(product_id) == context.clients[0]
+
+@then('un message RabbitMQ est envoyé avec les détails du client "{name} {quantity}')
+@patch('api.client_api.connect_rabbitmq')
+async def check_rabbitmq_message_sent(mock_connect_rabbitmq, name, quantity):
+    mock_channel = mock_connect_rabbitmq.return_value
+    mock_channel.basic_get.return_value = (None, None, f"client créé: {name} avec quantité: {quantity}".encode('utf-8'))
+
+    await create_client( client={"name": name, "quantity": quantity}, db=db())
+
+    mock_connect_rabbitmq.assert_called_once()
+    mock_channel.basic_publish.assert_called_once_with(exchange='', routing_key='client_queue', body=f"client créé: {name} avec quantité: {context.client_created.quantity}")
